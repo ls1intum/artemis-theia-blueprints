3afd470c70d42a80e7fbc14e742d2aac6d4aca79
Yannik Schmidt <yannik.schmidt@tum.de>
2024-11-26 11:41:09 +0100

    Remove VSC-Registry from Theia to prevent students from installing plugins

--- theia-extensions/product/src/browser/theia-ide-about-dialog.tsx
+++ theia-extensions/product/src/browser/theia-ide-about-dialog.tsx
@@ -7,100 +7,104 @@
  * SPDX-License-Identifier: MIT
  ********************************************************************************/
 
-import * as React from 'react';
-import { AboutDialog, AboutDialogProps, ABOUT_CONTENT_CLASS } from '@theia/core/lib/browser/about-dialog';
-import { injectable, inject } from '@theia/core/shared/inversify';
-import { renderDocumentation, renderDownloads, renderSourceCode, renderSupport, renderTickets, renderWhatIs } from './branding-util';
-import { VSXEnvironment } from '@theia/vsx-registry/lib/common/vsx-environment';
-import { WindowService } from '@theia/core/lib/browser/window/window-service';
+import * as React from "react";
+import {
+  AboutDialog,
+  AboutDialogProps,
+  ABOUT_CONTENT_CLASS,
+} from "@theia/core/lib/browser/about-dialog";
+import { injectable, inject } from "@theia/core/shared/inversify";
+import {
+  renderDocumentation,
+  renderDownloads,
+  renderSourceCode,
+  renderSupport,
+  renderTickets,
+  renderWhatIs,
+} from "./branding-util";
+//import { VSXEnvironment } from '@theia/vsx-registry/lib/common/vsx-environment';
+import { WindowService } from "@theia/core/lib/browser/window/window-service";
 
 @injectable()
 export class TheiaIDEAboutDialog extends AboutDialog {
+  //@inject(VSXEnvironment)
+  //protected readonly environment: VSXEnvironment;
 
-    @inject(VSXEnvironment)
-    protected readonly environment: VSXEnvironment;
+  @inject(WindowService)
+  protected readonly windowService: WindowService;
 
-    @inject(WindowService)
-    protected readonly windowService: WindowService;
+  protected vscodeApiVersion: string;
 
-    protected vscodeApiVersion: string;
+  constructor(
+    @inject(AboutDialogProps) protected readonly props: AboutDialogProps
+  ) {
+    super(props);
+  }
 
-    constructor(
-        @inject(AboutDialogProps) protected readonly props: AboutDialogProps
-    ) {
-        super(props);
-    }
+  protected async doInit(): Promise<void> {
+    this.vscodeApiVersion = "VSX-Env N/A"; //this.environment.getVscodeApiVersion();
+    super.doInit();
+  }
 
-    protected async doInit(): Promise<void> {
-        this.vscodeApiVersion = await this.environment.getVscodeApiVersion();
-        super.doInit();
-    }
+  protected render(): React.ReactNode {
+    return <div className={ABOUT_CONTENT_CLASS}>{this.renderContent()}</div>;
+  }
 
-    protected render(): React.ReactNode {
-        return <div className={ABOUT_CONTENT_CLASS}>
-            {this.renderContent()}
-        </div>;
-    }
+  protected renderContent(): React.ReactNode {
+    return (
+      <div className="ad-container">
+        <div className="ad-float">
+          <div className="ad-logo"></div>
+          {this.renderExtensions()}
+        </div>
+        {this.renderTitle()}
+        <hr className="gs-hr" />
+        <div className="flex-grid">
+          <div className="col">{renderWhatIs(this.windowService)}</div>
+        </div>
+        <div className="flex-grid">
+          <div className="col">{renderSupport(this.windowService)}</div>
+        </div>
+        <div className="flex-grid">
+          <div className="col">{renderTickets(this.windowService)}</div>
+        </div>
+        <div className="flex-grid">
+          <div className="col">{renderSourceCode(this.windowService)}</div>
+        </div>
+        <div className="flex-grid">
+          <div className="col">{renderDocumentation(this.windowService)}</div>
+        </div>
+        <div className="flex-grid">
+          <div className="col">{renderDownloads()}</div>
+        </div>
+      </div>
+    );
+  }
 
-    protected renderContent(): React.ReactNode {
-        return <div className='ad-container'>
-            <div className='ad-float'>
-                <div className='ad-logo'>
-                </div>
-                {this.renderExtensions()}
-            </div>
-            {this.renderTitle()}
-            <hr className='gs-hr' />
-            <div className='flex-grid'>
-                <div className='col'>
-                    {renderWhatIs(this.windowService)}
-                </div>
-            </div>
-            <div className='flex-grid'>
-                <div className='col'>
-                    {renderSupport(this.windowService)}
-                </div>
-            </div>
-            <div className='flex-grid'>
-                <div className='col'>
-                    {renderTickets(this.windowService)}
-                </div>
-            </div>
-            <div className='flex-grid'>
-                <div className='col'>
-                    {renderSourceCode(this.windowService)}
-                </div>
-            </div>
-            <div className='flex-grid'>
-                <div className='col'>
-                    {renderDocumentation(this.windowService)}
-                </div>
-            </div>
-            <div className='flex-grid'>
-                <div className='col'>
-                    {renderDownloads()}
-                </div>
-            </div>
-        </div>;
+  protected renderTitle(): React.ReactNode {
+    return (
+      <div className="gs-header">
+        <h1>
+          Eclipse Theia <span className="gs-blue-header">IDE</span>
+        </h1>
+        {this.renderVersion()}
+      </div>
+    );
+  }
 
-    }
+  protected renderVersion(): React.ReactNode {
+    return (
+      <div>
+        <p className="gs-sub-header">
+          {this.applicationInfo
+            ? "Version " + this.applicationInfo.version
+            : "-"}
+        </p>
 
-    protected renderTitle(): React.ReactNode {
-        return <div className='gs-header'>
-            <h1>Eclipse Theia <span className='gs-blue-header'>IDE</span></h1>
-            {this.renderVersion()}
-        </div>;
-    }
-
-    protected renderVersion(): React.ReactNode {
-        return <div>
-            <p className='gs-sub-header' >
-                {this.applicationInfo ? 'Version ' + this.applicationInfo.version : '-'}
-            </p>
-
-            <p className='gs-sub-header' >
-                {'VS Code API Version: ' + this.vscodeApiVersion}
-            </p>
-        </div>;
-    }
+        <p className="gs-sub-header">
+          {"VS Code API Version: " + this.vscodeApiVersion}
+        </p>
+      </div>
+    );
+  }
 }
