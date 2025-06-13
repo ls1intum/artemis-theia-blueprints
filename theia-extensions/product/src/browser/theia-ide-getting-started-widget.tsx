f3ebeec85be9aca9279bbea321efd3b1bcdef5b8
Auxburger <darius.augsburger@redpoint.teseon.com>
2025-06-13 10:02:11 +0200

    adjust started widget

--- theia-extensions/product/src/browser/theia-ide-getting-started-widget.tsx
+++ theia-extensions/product/src/browser/theia-ide-getting-started-widget.tsx
@@ -12,14 +12,14 @@ import * as React from "react";
 import { Message, PreferenceService } from "@theia/core/lib/browser";
 import { inject, injectable } from "@theia/core/shared/inversify";
 import {
-  renderDocumentation,
-  renderDownloads,
-  renderExtendingCustomizing,
-  renderSourceCode,
-  renderSupport,
-  renderTickets,
-  renderWhatIs,
-  renderCollaboration,
+	renderDocumentation,
+	renderDownloads,
+	renderExtendingCustomizing,
+	renderSourceCode,
+	renderSupport,
+	renderTickets,
+	renderWhatIs,
+	renderCollaboration,
 } from "./branding-util";
 
 import { GettingStartedWidget } from "@theia/getting-started/lib/browser/getting-started-widget";
@@ -28,192 +28,140 @@ import { WindowService } from "@theia/core/lib/browser/window/window-service";
 
 @injectable()
 export class TheiaIDEGettingStartedWidget extends GettingStartedWidget {
-  //@inject(VSXEnvironment)
-  //protected readonly environment: VSXEnvironment;
+	//@inject(VSXEnvironment)
+	//protected readonly environment: VSXEnvironment;
 
-  @inject(WindowService)
-  protected readonly windowService: WindowService;
+	@inject(WindowService)
+	protected readonly windowService: WindowService;
 
-  @inject(PreferenceService)
-  protected readonly preferenceService: PreferenceService;
+	@inject(PreferenceService)
+	protected readonly preferenceService: PreferenceService;
 
-  protected vscodeApiVersion: string;
+	protected vscodeApiVersion: string;
 
-  protected async doInit(): Promise<void> {
-    super.doInit();
-    this.vscodeApiVersion = "VSX-Env N/A"; //await this.environment.getVscodeApiVersion();
-    await this.preferenceService.ready;
-    this.update();
-  }
+	protected async doInit(): Promise<void> {
+		super.doInit();
+		this.vscodeApiVersion = "VSX-Env N/A"; //await this.environment.getVscodeApiVersion();
+		await this.preferenceService.ready;
+		this.update();
+	}
 
-  protected onActivateRequest(msg: Message): void {
-    super.onActivateRequest(msg);
-    const htmlElement = document.getElementById("alwaysShowWelcomePage");
-    if (htmlElement) {
-      htmlElement.focus();
-    }
-  }
+	protected onActivateRequest(msg: Message): void {
+		super.onActivateRequest(msg);
+		const htmlElement = document.getElementById("alwaysShowWelcomePage");
+		if (htmlElement) {
+			htmlElement.focus();
+		}
+	}
 
-  protected render(): React.ReactNode {
-    return (
-      <div className="gs-container">
-        <div className="gs-content-container">
-          <div className="gs-float">
-            <div className="gs-logo"></div>
-            {this.renderActions()}
-          </div>
-          {this.renderHeader()}
-          <hr className="gs-hr" />
-          <div className="flex-grid">
-            <div className="col">{renderWhatIs(this.windowService)}</div>
-          </div>
-          <div className="flex-grid">
-            <div className="col">
-              {renderExtendingCustomizing(this.windowService)}
-            </div>
-          </div>
-          <div className="flex-grid">
-            <div className="col">{renderSupport(this.windowService)}</div>
-          </div>
-          <div className="flex-grid">
-            <div className="col">{renderTickets(this.windowService)}</div>
-          </div>
-          <div className="flex-grid">
-            <div className="col">{renderSourceCode(this.windowService)}</div>
-          </div>
-          <div className="flex-grid">
-            <div className="col">{renderDocumentation(this.windowService)}</div>
-          </div>
-          <div className="flex-grid">
-            <div className="col">{this.renderAIBanner()}</div>
-          </div>
-          <div className="flex-grid">
-            <div className="col">{renderCollaboration(this.windowService)}</div>
-          </div>
-          <div className="flex-grid">
-            <div className="col">{renderDownloads()}</div>
-          </div>
-        </div>
-        <div className="gs-preference-container">
-          {this.renderPreferences()}
-        </div>
-      </div>
-    );
-  }
+	protected render(): React.ReactNode {
+		return (
+			<div className="gs-container">
+				<div className="gs-content-container">
+					<div className="gs-float">
+						<div className="gs-logo"></div>
+						{this.renderActions()}
+					</div>
+					{this.renderHeader()}
+					<hr className="gs-hr" />
+					<div className="flex-grid">
+						<div className="col">{this.renderNews()}</div>
+					</div>
+					<div className="flex-grid">
+						<div className="col">{renderWhatIs(this.windowService)}</div>
+					</div>
+					<div className="flex-grid">
+						<div className="col">
+							{renderExtendingCustomizing(this.windowService)}
+						</div>
+					</div>
+					<div className="flex-grid">
+						<div className="col">{renderSupport(this.windowService)}</div>
+					</div>
+					<div className="flex-grid">
+						<div className="col">{renderTickets(this.windowService)}</div>
+					</div>
+					<div className="flex-grid">
+						<div className="col">{renderSourceCode(this.windowService)}</div>
+					</div>
+					<div className="flex-grid">
+						<div className="col">{renderDocumentation(this.windowService)}</div>
+					</div>
+					<div className="flex-grid">
+						<div className="col">{this.renderAIBanner()}</div>
+					</div>
+					<div className="flex-grid">
+						<div className="col">{renderCollaboration(this.windowService)}</div>
+					</div>
+					<div className="flex-grid">
+						<div className="col">{renderDownloads()}</div>
+					</div>
+				</div>
+				<div className="gs-preference-container">
+					{this.renderPreferences()}
+				</div>
+			</div>
+		);
+	}
 
-  protected renderActions(): React.ReactNode {
-    return (
-      <div className="gs-container">
-        <div className="flex-grid">
-          <div className="col">{this.renderStart()}</div>
-        </div>
-        <div className="flex-grid">
-          <div className="col">{this.renderRecentWorkspaces()}</div>
-        </div>
-        <div className="flex-grid">
-          <div className="col">{this.renderSettings()}</div>
-        </div>
-        <div className="flex-grid">
-          <div className="col">{this.renderHelp()}</div>
-        </div>
-      </div>
-    );
-  }
+	protected renderActions(): React.ReactNode {
+		return (
+			<div className="gs-container">
+				<div className="flex-grid">
+					<div className="col">{this.renderStart()}</div>
+				</div>
+				<div className="flex-grid">
+					<div className="col">{this.renderRecentWorkspaces()}</div>
+				</div>
+				<div className="flex-grid">
+					<div className="col">{this.renderSettings()}</div>
+				</div>
+				<div className="flex-grid">
+					<div className="col">{this.renderHelp()}</div>
+				</div>
+			</div>
+		);
+	}
 
-  protected renderHeader(): React.ReactNode {
-    return (
-      <div className="gs-header">
-        <h1>
-          Eclipse Theia <span className="gs-blue-header">IDE</span>
-        </h1>
-        {this.renderVersion()}
-      </div>
-    );
-  }
+	protected renderHeader(): React.ReactNode {
+		return (
+			<div className="gs-header">
+				<h1>
+					Eclipse Theia <span className="gs-blue-header">IDE</span>
+				</h1>
+				{this.renderVersion()}
+			</div>
+		);
+	}
 
-  protected renderVersion(): React.ReactNode {
-    return (
-      <div>
-        <p className="gs-sub-header">
-          {this.applicationInfo
-            ? "Version " + this.applicationInfo.version
-            : "-"}
-        </p>
+	protected renderVersion(): React.ReactNode {
+		return (
+			<div>
+				<p className="gs-sub-header">
+					{this.applicationInfo
+						? "Version " + this.applicationInfo.version
+						: "-"}
+				</p>
 
-        <p className="gs-sub-header">
-          {"VS Code API Version: " + this.vscodeApiVersion}
-        </p>
-      </div>
-    );
-  }
+				<p className="gs-sub-header">
+					{"VS Code API Version: " + this.vscodeApiVersion}
+				</p>
+			</div>
+		);
+	}
 
-  protected renderAIBanner(): React.ReactNode {
-    return (
-      <div className="gs-section">
-        <div className="flex-grid">
-          <div className="col">
-            <h3 className="gs-section-header">
-              {" "}
-              🚀 AI Support in the Theia IDE is available! [Experimental] ✨
-            </h3>
-
-            <div className="gs-action-container">
-              Theia IDE now contains experimental AI support, which offers early
-              access to cutting-edge AI capabilities within your IDE.
-              <br />
-              Please note that these features are disabled by default, ensuring
-              that users can opt-in at their discretion. For those who choose to
-              enable AI support, it is important to be aware that these
-              experimental features may generate continuous requests to the
-              language models (LLMs) you provide access to. This might incur
-              costs that you need to monitor closely.
-              <br />
-              For more details, please visit &nbsp;
-              <a
-                role={"button"}
-                tabIndex={0}
-                onClick={() => this.doOpenExternalLink(this.theiaAIDocUrl)}
-                onKeyDown={(e: React.KeyboardEvent) =>
-                  this.doOpenExternalLinkEnter(e, this.theiaAIDocUrl)
-                }
-              >
-                {"the documentation"}
-              </a>
-              .
-              <br />
-              <br />
-              🚧 Please note that this feature is currently in development and
-              may undergo frequent changes. We welcome your feedback,
-              contributions, and sponsorship! To support the ongoing development
-              of the AI capabilities please visit the&nbsp;
-              <a
-                role={"button"}
-                tabIndex={0}
-                onClick={() => this.doOpenExternalLink(this.ghProjectUrl)}
-                onKeyDown={(e: React.KeyboardEvent) =>
-                  this.doOpenExternalLinkEnter(e, this.ghProjectUrl)
-                }
-              >
-                {"Github Project"}
-              </a>
-              . &nbsp;Thank you for being part of our community!
-            </div>
-            <div className="gs-action-container">
-              <a
-                role={"button"}
-                style={{ fontSize: "var(--theia-ui-font-size2)" }}
-                tabIndex={0}
-                onClick={() => this.doOpenAIChatView()}
-                onKeyDown={(e: React.KeyboardEvent) =>
-                  this.doOpenAIChatViewEnter(e)
-                }
-              >
-                {"Open the AI Chat View now to learn how to start! ✨"}
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+	protected renderAIBanner(): React.ReactNode {
+		const framework = super.renderAIBanner();
+		if (
+			React.isValidElement<
+				React.DetailedHTMLProps<
+					React.HTMLAttributes<HTMLDivElement>,
+					HTMLDivElement
+				>
+			>(framework)
+		) {
+			return React.cloneElement(framework, { className: "gs-section" });
+		}
+		return framework;
+	}
 }
