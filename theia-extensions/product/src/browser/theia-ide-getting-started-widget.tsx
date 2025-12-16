50e8e3b68bb941a6fb5a285582c8611ffef1f33d
Fangxing Liu <fx.liu@tum.de>
2025-12-16 01:06:59 +0100

    feat: add custom start banner for onboarding

--- theia-extensions/product/src/browser/theia-ide-getting-started-widget.tsx
+++ theia-extensions/product/src/browser/theia-ide-getting-started-widget.tsx
@@ -9,7 +9,7 @@
 
 import * as React from "react";
 
-import { Message } from "@theia/core/lib/browser";
+import { codicon, Message } from "@theia/core/lib/browser";
 import { inject, injectable } from "@theia/core/shared/inversify";
 import {
   renderTickets,
@@ -18,6 +18,7 @@ import {
 
 import { GettingStartedWidget } from "@theia/getting-started/lib/browser/getting-started-widget";
 import { WindowService } from "@theia/core/lib/browser/window/window-service";
+import { environment, isOSX, nls } from "@theia/core";
 
 @injectable()
 export class TheiaIDEGettingStartedWidget extends GettingStartedWidget {
@@ -86,4 +87,83 @@ export class TheiaIDEGettingStartedWidget extends GettingStartedWidget {
     );
   }
 
+    /**
+     * Render the `Start` section.
+     * Displays a collection of "start-to-work" related commands like `open` commands and some other.
+     */
+  protected override renderStart(): React.ReactNode {
+      const requireSingleOpen = isOSX || !environment.electron.is();
+      const openFileExplorer = requireSingleOpen && <div className='gs-action-container'>
+          <a
+              role={'button'}
+              tabIndex={0}
+              onClick={this.doOpenExplorer}
+              onKeyDown={this.doOpenExplorerEnter}>
+              {nls.localizeByDefault('Open Exercise in Explorer View')}
+          </a>
+      </div>;
+      const openSourceControl = requireSingleOpen && <div className='gs-action-container'>
+          <a
+              role={'button'}
+              tabIndex={0}
+              onClick={this.doOpenScm}
+              onKeyDown={this.doOpenScmEnter}>
+              {nls.localizeByDefault('Open Source Control View')}
+          </a>
+      </div>;
+      const showAllTerminals = requireSingleOpen && <div className='gs-action-container'>
+          <a
+              role={'button'}
+              tabIndex={0}
+              onClick={this.doToggleTermials}
+              onKeyDown={this.doToggleTermialsEnter}>
+              {nls.localizeByDefault('Open Terminal View')}
+          </a>
+      </div>;
+
+      return <div className='gs-section'>
+          <h3 className='gs-section-header'><i className={codicon('folder-opened')}></i>{nls.localizeByDefault('Start')}</h3>
+          {openFileExplorer}
+          {openSourceControl}
+          {showAllTerminals}
+      </div>;
+  }
+
+  /**
+  * Trigger the view explorer command.
+  */
+  protected doOpenExplorer = () => this.commandRegistry.executeCommand('workbench.view.explorer');
+  protected doOpenExplorerEnter = (e: React.KeyboardEvent) => {
+      if (this.isEnterKey(e)) {
+          this.doOpenExplorer();
+      }
+  };
+  protected test = () => {
+    console.log('start listing all commands')
+    const iterator = this.commandRegistry.getAllCommands();
+    for (let out = iterator.next(); !out.done; out = iterator.next()) {
+      console.log('command', out.value);
+    }
+    console.log('finish listing all commands')
+  }
+
+  /**
+  * Trigger the view source control manager command.
+  */
+  protected doOpenScm = () => this.commandRegistry.executeCommand('scmView:toggle');
+  protected doOpenScmEnter = (e: React.KeyboardEvent) => {
+    if (this.isEnterKey(e)) {
+        this.doOpenScm();
+    }
+  };
+
+  /**
+  * Trigger the toggle terminal command.
+  */
+  protected doToggleTermials = () => this.commandRegistry.executeCommand('workbench.action.terminal.toggleTerminal');
+  protected doToggleTermialsEnter = (e: React.KeyboardEvent) => {
+      if (this.isEnterKey(e)) {
+          this.doToggleTermials();
+      }
+  }
 }
