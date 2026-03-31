d64413dd2d3c02c4ef7ead43a96ad63e90a4f99d
= <kevingruber01@protonmail.com>
2026-03-31 17:37:01 +0200

    Fix code rabbit issues

--- images/base-ide/entrypoint.sh
+++ images/base-ide/entrypoint.sh
@@ -2,12 +2,18 @@
 set -e
 
 if [ "$STANDALONE_MODE" = "true" ]; then
-    TEMPLATE="/home/theia/templates/${BUILD_SYSTEM}"
-    if [ -d "$TEMPLATE" ]; then
-        cp -rn "$TEMPLATE/." /home/project/
-        # Make gradlew executable if present (cp does not preserve execute bit from COPY)
-        [ -f /home/project/gradlew ] && chmod +x /home/project/gradlew
+    if [ -z "$BUILD_SYSTEM" ]; then
+        echo "ERROR: STANDALONE_MODE is set but BUILD_SYSTEM is unset or empty" >&2
+        exit 1
     fi
+    TEMPLATE="/home/theia/templates/${BUILD_SYSTEM}"
+    if [ ! -d "$TEMPLATE" ]; then
+        echo "ERROR: No template found for BUILD_SYSTEM='${BUILD_SYSTEM}' (looked in ${TEMPLATE})" >&2
+        exit 1
+    fi
+    cp -rn "$TEMPLATE/." /home/project/
+    # Make gradlew executable if present (cp does not preserve execute bit from COPY)
+    [ -f /home/project/gradlew ] && chmod +x /home/project/gradlew
 fi
 
 exec node /home/theia/applications/browser/lib/backend/main.js "$@"
