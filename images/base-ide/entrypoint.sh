88190be919610b8bd6b7f762a669363db059ec5a
= <kevingruber01@protonmail.com>
2026-03-31 17:31:51 +0200

    Minor adjustments to the wd

--- images/base-ide/entrypoint.sh
+++ images/base-ide/entrypoint.sh
@@ -5,6 +5,8 @@ if [ "$STANDALONE_MODE" = "true" ]; then
     TEMPLATE="/home/theia/templates/${BUILD_SYSTEM}"
     if [ -d "$TEMPLATE" ]; then
         cp -rn "$TEMPLATE/." /home/project/
+        # Make gradlew executable if present (cp does not preserve execute bit from COPY)
+        [ -f /home/project/gradlew ] && chmod +x /home/project/gradlew
     fi
 fi
 
